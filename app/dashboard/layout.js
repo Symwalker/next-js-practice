269d6d750a8217a99ddd339a9208837006cffe3d
user name <99411978+Symwalker@users.noreply.github.com>
2023-12-18 14:53:57 +0500

    first commit

---
+++ app/dashboard/layout.js
@@ -0,0 +1,11 @@
+export default function DashboardLayout({ children }) {
+    return (
+      <html lang="en">
+        <body >
+          <div>
+            this is Dashboard Layout
+          </div>
+          {children}</body>
+      </html>
+    )
+  }
